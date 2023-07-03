5a4b62982987d07fb52dc42722513d8cd450d310
Gabrijel Bartosek <Gabrijel.Bartosek@external.t-systems.com>
2023-07-03 13:39:32 +0200

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Gabrijel Bartosek on 03.07.2023..
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
